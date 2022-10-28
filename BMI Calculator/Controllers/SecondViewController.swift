dedd7635b31a68f110fd4a7accad7e61f7a66932
Djordje Knezevic <starwarrior@mycity.rs>
2022-10-28 18:37:05 +0200

    Add SecondViewController file and create custom UIViewController class in it

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Djordje Knezevic on 28.10.22..
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+      
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

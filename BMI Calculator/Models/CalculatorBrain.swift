d3a8a84d950a7a1ad7b2c13f53b8cb6c5293a401
Djordje Knezevic <starwarrior@mycity.rs>
2022-11-01 21:11:18 +0100

    Set various advice and background color in ResultViewController depending on calculated BMI value in CalculatorBrain and finish project

--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -6,21 +6,35 @@
 //  Copyright © 2022 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmi: Float = 0.0
+    var bmi: BMI?
     
     func getBMIValue() -> String {
-        let formatedBMI = String(format: "%.1f", bmi)
+        let formatedBMI = String(format: "%.1f", bmi?.value ?? 0.0)
         return formatedBMI
     }
     
-    mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / pow(height, 2)
+    func getAdvice() -> String {
+        return bmi?.advice ?? ""
     }
     
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.white
+    }
     
+    mutating func calculateBMI(height: Float, weight: Float) {
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor(red: 0.60, green: 1.00, blue: 1.00, alpha: 1.00))
+        } else if bmiValue <= 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: UIColor(red: 0.60, green: 1.00, blue: 0.60, alpha: 1.00))
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor(red: 1.00, green: 0.60, blue: 0.60, alpha: 1.00))
+        }
+    }
 }
 
